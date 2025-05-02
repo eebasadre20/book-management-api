89ec2deaaef733259d3290fc24ff5343b416d62d
Korie (Jitera) <ai_developers@jitera.com>
2025-05-02 12:57:32 +0000

    feat: add To-Do entity to the project

---
+++ src/entities/todo.entity.ts
@@ -0,0 +1,23 @@
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('todos')
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @Column({ type: 'boolean', default: false })
+  isCompleted: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', nullable: true, onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt?: Date;
+}
